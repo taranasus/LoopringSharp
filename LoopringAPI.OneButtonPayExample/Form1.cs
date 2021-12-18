ea2563c4bc6f0424816f9c8feecc0183bcfa4d85
Kaizen Taranasus <taranasus@gmail.com>
2021-12-18 14:48:54 +0000

    Small change

--- LoopringAPI.OneButtonPayExample/Form1.cs
+++ LoopringAPI.OneButtonPayExample/Form1.cs
@@ -11,7 +11,9 @@ namespace LoopringAPI.OneButtonPayExample
 
         private void button1_Click(object sender, EventArgs e)
         {
-                LoopringAPI.Client client = new Client("https://uat2.loopring.io/", "0x452386e0516cc1600e9f43c719d0c80c6abc51f9", File.ReadAllText("l2Pk.txt"), true);
+                // You need to create two files (ethAddress.txt and l2Pk.txt) containing your ethereum public address and your loopring private key respectively
+                // This is a temporary inconvenience that I'm working on sorting out in the future, so all that you put in is the loopring api url and nothing more
+                LoopringAPI.Client client = new Client("https://uat2.loopring.io/", File.ReadAllText("ethAddress.txt"), File.ReadAllText("l2Pk.txt"), true);
                 var result = client.Transfer("0x2e76ebd1c7c0c8e7c2b875b6d505a260c525d25e", "LRC", 1, "LRC", "Potatoes are nice").Result;
                 MessageBox.Show("Thanks for the monye honey!");
             
